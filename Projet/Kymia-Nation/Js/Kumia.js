0be1b04ad7362c8f84ee50087ca0e979a8d97d8d
XXXXX <xxxxx@example.com>
2021-07-15 00:15:22 +0200

    Ajout du projet

---
+++ Projet/Kymia-Nation/Js/Kumia.js
@@ -0,0 +1,20 @@
+let music = document.getElementById("music");
+let togg2 = document.getElementById("togg2");
+let d1 = document.getElementById("d1");
+let d2 = document.getElementById("d2");
+music.addEventListener("click", () => {
+  if(getComputedStyle(d1).display != "none"){
+    d1.style.display = "none";
+  } else {
+    d1.style.display = "block";
+  }
+})
+
+function togg(){
+  if(getComputedStyle(d2).display != "none"){
+    d2.style.display = "none";
+  } else {
+    d2.style.display = "block";
+  }
+};
+togg2.onclick = togg;
